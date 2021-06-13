1d320019a5d2aaac69fd0d197db819e18890c788
thisisharrison <harrison.lau@outlook.com>
2021-06-13 23:56:42 +0800

    deploy to github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
+const webpack = require("webpack");
+const DevServer = require("webpack-dev-server");
+const HTMLPlugin = require("html-webpack-plugin");
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const env = {
+    src: resolve("src"),
+    tsConfig: resolve("tsconfig.json"),
+    stylelintConfig: resolve("stylelint.json"),
+    tslintConfig: resolve("tslint.json"),
+};
+
+function resolve(relativePath) {
+    return path.resolve(__dirname, `./${relativePath}`);
+}
 
 module.exports = {
-    entry: "./src/index.tsx",
+    mode: "production",
+    entry: [`${env.src}/index.tsx`],
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js",
     },
     resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        modules: [env.src, "node_modules"],
     },
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.(ts|tsx)$/,
-    //             loader: "awesome-typescript-loader",
-    //         },
-    //         {
-    //             enforce: "pre",
-    //             test: /\.js$/,
-    //             loader: "source-map-loader",
-    //         },
-    //         {
-    //             test: /\.css$/,
-    //             loader: "css-loader",
-    //         },
-    //     ],
-    // },
+    devtool: "cheap-module-source-map",
+    optimization: {
+        splitChunks: {
+            automaticNameDelimiter: "-",
+            maxAsyncRequests: 10,
+        },
+    },
     module: {
         rules: [
             {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-                    },
+                test: /\.(ts|tsx|js|jsx)$/,
+                include: env.src,
+                loader: "ts-loader",
+                options: {
+                    configFile: env.tsConfig,
+                    transpileOnly: true,
                 },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html"),
-        }),
-    ],
+    plugins: [new HTMLPlugin({template: `${env.src}/index.html`}), new webpack.HotModuleReplacementPlugin()],
 };
